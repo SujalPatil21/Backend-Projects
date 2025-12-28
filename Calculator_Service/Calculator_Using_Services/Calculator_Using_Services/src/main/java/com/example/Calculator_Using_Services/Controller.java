4129e55a6d3ea7a732570851fcb30af0caa40fda
SujalPatil21 <sujalbpatil21@gmail.com>
2025-12-28 19:33:53 +0530

    Calculator Using Service Class

---
+++ Calculator_Service/Calculator_Using_Services/Calculator_Using_Services/src/main/java/com/example/Calculator_Using_Services/Controller.java
@@ -0,0 +1,42 @@
+package com.example.Calculator_Using_Services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    private final ArithmeticService service;
+
+    public Controller(ArithmeticService service){
+
+        this.service = service ;
+    }
+
+    @GetMapping("/calc/add")
+    public String add(@RequestParam int a , @RequestParam int b){
+
+        return "Addition : " + service.add(a,b);
+    }
+
+    @GetMapping("/calc/sub")
+    public String sub(@RequestParam int a , @RequestParam int b){
+
+        return "Subtraction : " + service.sub(a,b);
+    }
+
+    @GetMapping("/calc/mul")
+    public String mul(@RequestParam int a , @RequestParam int b){
+
+        return "Multiplication : " + service.mul(a,b);
+    }
+
+    @GetMapping("/calc/div")
+    public String div(@RequestParam int a , @RequestParam int b){
+
+        return "Division : " + service.div(a,b);
+    }
+
+}
---
+++ Calculator_Service/Calculator_Using_Services/Calculator_Using_Services/src/test/java/com/example/Calculator_Using_Services/CalculatorUsingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Calculator_Using_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorUsingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
